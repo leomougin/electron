23ac0d88a7490bbffa70d400e0d9233c6fa11d69
Léo Mougin <mougin.leo@yahoo.com>
2025-03-11 14:52:17 +0100

    ajout menu personnalisé

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 // Processus principal
 
-const {app,BrowserWindow,ipcMain} = require('electron')
+const {app,BrowserWindow,ipcMain,Menu} = require('electron')
 const path = require('path')
+let window
 
 // Créer la fenêtre principale
 function createWindow(){
-    const window = new BrowserWindow({
+    window = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
@@ -15,7 +16,8 @@ function createWindow(){
             preload: path.join(__dirname, '/src/js/preload.js')
         }
     })
-
+    // Création du menu
+    createMenu()
     // Charger index.html
     window.loadFile('src/pages/index.html')
 }
@@ -40,6 +42,53 @@ ipcMain.handle('get-versions',()=> {
     }
 })
 
+// Fonction permettant de créer un menu personnalisé
+function createMenu() {
+    // Créet un tableau qui va représenter le menu -> modèle
+    const template =
+        [
+            {
+                label:'App',
+                submenu:
+                    [
+                        {
+                            label:'Version',
+                            click: () =>window.loadFile('src/pages/index.html')
+                        },
+                        {
+                          type:'separator'
+                        },
+                        {
+                            label:'Quitter',
+                            accelerator:process.platform === 'darwin' ? 'Cmd+Q':'Ctrl+Q',
+                            click:()=>app.quit()
+                        }
+                ]
+            },
+            {
+                label: 'Tâches',
+                submenu:
+                [
+                    {
+                        label:'Liste',
+                        click: () =>window.loadFile('src/pages/listeTache.html')
+                    },
+                    {
+                        type:'separator'
+                    },
+                    {
+                        label:'Ajouter',
+                        click: () =>window.loadFile('src/pages/ajouterTache.html')
+                    }
+                ]
+            }
+        ]
+
+    // Créer le menu à partir du modèle
+    const menu = Menu.buildFromTemplate(template)
+    // Définir le menu comme menu de l'application
+    Menu.setApplicationMenu(menu)
+}
 // Fermer l'application si les fenêtres sont fermées
 // Sur MacOS, les applications ne se ferment pas lorsqu'on ferme la dernière fenêtre
 app.on('window-all-closed', () => {
